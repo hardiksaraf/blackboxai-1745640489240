929f193856002a0e288dd39e6108711ee0885b08
Hardik Saraf <hardiksaraf18@gmail.com>
2025-04-26 10:26:33 +0530

    Add notifications/admin.py 1745643393036

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Notification, ChannelPreference
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'type', 'message', 'read', 'created_at')
+    list_filter = ('type', 'read', 'created_at')
+    search_fields = ('user__email', 'message')
+
+@admin.register(ChannelPreference)
+class ChannelPreferenceAdmin(admin.ModelAdmin):
+    list_display = ('user', 'email_enabled', 'sms_enabled', 'push_enabled')
+    search_fields = ('user__email',)
