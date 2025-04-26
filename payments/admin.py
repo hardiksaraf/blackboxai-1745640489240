ee1b4c8dc3e856649175e1ee77840aaf0d0fd5cd
Hardik Saraf <hardiksaraf18@gmail.com>
2025-04-26 10:27:19 +0530

    Add payments/admin.py 1745643439148

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Transaction, Refund, PaymentMethod
 
-# Register your models here.
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'booking', 'customer', 'amount', 'currency', 'status', 'gateway', 'payment_time')
+    list_filter = ('status', 'gateway', 'payment_time')
+    search_fields = ('customer__email', 'booking__id')
+    ordering = ('-payment_time',)
+
+@admin.register(Refund)
+class RefundAdmin(admin.ModelAdmin):
+    list_display = ('id', 'transaction', 'amount', 'status', 'processed_at')
+    list_filter = ('status', 'processed_at')
+    search_fields = ('transaction__id',)
+
+@admin.register(PaymentMethod)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'method_type', 'is_active')
+    list_filter = ('method_type', 'is_active')
+    search_fields = ('customer__email', 'gateway_reference')
