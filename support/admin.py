7a151160e72a088455c7c8aa0c9143a06660c433
Hardik Saraf <hardiksaraf18@gmail.com>
2025-04-26 10:27:10 +0530

    Add support/admin.py 1745643430125

--- support/admin.py
+++ support/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import SupportTicket, TicketMessage, Category
 
-# Register your models here.
+@admin.register(SupportTicket)
+class SupportTicketAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'subject', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('user__email', 'subject')
+
+@admin.register(TicketMessage)
+class TicketMessageAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'sender', 'timestamp')
+    search_fields = ('sender__email', 'message')
+    ordering = ('-timestamp',)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
