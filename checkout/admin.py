c81d477a37d8a6f4d63b4825a3907904aae99062
Hardik Saraf <hardiksaraf18@gmail.com>
2025-04-26 10:27:38 +0530

    Add checkout/admin.py 1745643457833

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cart, OrderSummary
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'created_at')
+    search_fields = ('customer__email',)
+    ordering = ('-created_at',)
+
+@admin.register(OrderSummary)
+class OrderSummaryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'customer', 'booking', 'final_price', 'coupon_applied', 'summary_time')
+    search_fields = ('customer__email', 'booking__id')
+    ordering = ('-summary_time',)
