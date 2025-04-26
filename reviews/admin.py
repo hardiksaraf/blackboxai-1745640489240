06e68e15ee395ce6dbe12f39f7c0feb4e8445092
Hardik Saraf <hardiksaraf18@gmail.com>
2025-04-26 10:27:17 +0530

    Add reviews/admin.py 1745643436893

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Review, ReviewComment
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'venue', 'rating', 'is_approved', 'created_at')
+    list_filter = ('is_approved', 'rating', 'created_at')
+    search_fields = ('customer__email', 'venue__title', 'comment')
+    ordering = ('-created_at',)
+
+@admin.register(ReviewComment)
+class ReviewCommentAdmin(admin.ModelAdmin):
+    list_display = ('review', 'commenter', 'timestamp')
+    search_fields = ('commenter__email', 'comment_text')
+    ordering = ('-timestamp',)
